0f72928340d9d16d8936b2a87eb57538ad4f217d
PRANAY HARISHCHANDRA <pranayharishchandra@gmail.com>
2023-08-24 02:24:09 +0530

    igonre this

--- src/App.js
+++ src/App.js
@@ -2,18 +2,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 
 
-
-
-
-
-
-
-
 function App() {
   return (
     <Router>
       <div className='container'>
-        <h1 className=' mx-auto px-3 pb-12'>helo</h1>
+        <h1 className=' mx-auto px-3 pb-12'>helo</h1> 
       </div>
       
     </Router>
